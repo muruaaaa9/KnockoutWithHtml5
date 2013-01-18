fa0b1118b7326fe770c24c7b21622e5331d996e9
Murugan Amir <murugan.amir@bglgroup.co.uk>
2013-01-18 14:54:39 +0000

    Added the initial version check in

---
+++ Src/KnockoutWithHtml5.Web/Customer Scripts/Home.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../Scripts/knockout-2.2.1.debug.js" />
+
+
+$(function () {
+    var product = {
+        id: 1001,
+        itemNumber: "TYA1001",
+        model: "Totyoa",
+        salePrice: "20000"
+    };
+
+    $("#itemnumber").text(product.id);
+    $("#model").val(product.model);
+    $("#saleprice").val(product.salePrice);
+    //KNOCKOUT BINDING
+    var productKO = {
+        id: 1001,
+        itemNumber: ko.observable("TYA1001"),
+        model: ko.observable("Totyoa"),
+        salePrice: ko.observable("20000")
+    };
+
+    ko.applyBindings(productKO);
+
+});
